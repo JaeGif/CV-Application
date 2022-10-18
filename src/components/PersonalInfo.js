0da9fc2fbb6806efed4fb5294640ca176c374163
Jae Gifford <jae.giff@outlook.com>
2022-10-18 12:56:53 +0800

    bounded sizes for input areas

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -18,44 +18,55 @@ export class PersonalInfo extends Component {
 
   render() {
     return (
-      <form id={styles.formStructure}>
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='firstName'>First Name</label>
-          <input name='firstName' type='text' />
-        </div>
+      <form>
+        <div id={styles.formStructure}>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='firstName'>First Name</label>
+            <input className={styles.inputBoxes} name='firstName' type='text' />
+          </div>
 
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='lastName'>Last Name</label>
-          <input name='lastName' type='text' />
-        </div>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='lastName'>Last Name</label>
+            <input className={styles.inputBoxes} name='lastName' type='text' />
+          </div>
 
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='title'>Job Title</label>
-          <input name='title' type='text' placeholder='Desired title' />
-        </div>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='title'>Job Title</label>
+            <input
+              className={styles.inputBoxes}
+              name='title'
+              type='text'
+              placeholder='Desired title'
+            />
+          </div>
 
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='address'>Address</label>
-          <input name='address' type='text' />
-        </div>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='address'>Address</label>
+            <input className={styles.inputBoxes} name='address' type='text' />
+          </div>
 
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='phone'>Phone Number</label>
-          <input name='phone' type='text' />
-        </div>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='phone'>Phone Number</label>
+            <input className={styles.inputBoxes} name='phone' type='number' />
+          </div>
 
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='email'>Email</label>
-          <input name='email' type='text' />
-        </div>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='email'>Email</label>
+            <input className={styles.inputBoxes} name='email' type='email' />
+          </div>
 
-        <div className={styles.inputLabelsWrapper}>
-          <label htmlFor='summary'>Summary</label>
-          <textarea
-            name='summary'
-            type='text'
-            placeholder='Summary of your skills'
-          ></textarea>
+          <div className={styles.inputLabelsWrapper}>
+            <label htmlFor='summary'>Summary</label>
+            <textarea
+              className={styles.textAreas}
+              name='summary'
+              type='text'
+              placeholder='Summary of your skills'
+            ></textarea>
+          </div>
+          <button className={styles.nextButtons} type='submit'>
+            Next
+          </button>
         </div>
       </form>
     );

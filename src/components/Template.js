2c2839013a01f2d3ffd2cc2a9120e794b78916f1
Jae Gifford <jae.giff@outlook.com>
2022-10-19 12:45:41 +0800

    staging flex text changes

--- src/components/Template.js
+++ src/components/Template.js
@@ -6,9 +6,17 @@ export class Template extends Component {
   render() {
     return (
       <div style={TemplateWrapperStyles}>
-        <div style={nameWrapperStyles}>
-          <h1>Jae</h1>
-          <h1>Gifford</h1>
+        <div style={summaryDisplayStyles}>
+          <div style={nameWrapperStyles}>
+            <h1 className={styles.headingText}>Jae</h1>
+            <h1 className={styles.headingText}>Gifford</h1>
+          </div>
+          <p className={`${styles.regularText} ${styles.wrappingBodyText}`}>
+            This is a summary of Jae, the best dude for the job. You know it,
+            everyone knows it. I hope that the world will know it soon. Ya boi
+            makes good websites. He also wanted to add enough text that the
+            element would overflow so that he knows the exact limitations
+          </p>
         </div>
       </div>
     );
@@ -19,7 +27,7 @@ export default Template;
 
 const TemplateWrapperStyles = {
   height: '80vh',
-  width: '57vh',
+  width: '30vw',
   border: 'solid grey 2px',
   backgroundImage: `url(${background})`,
   backgroundRepeat: 'no-repeat',
@@ -27,7 +35,16 @@ const TemplateWrapperStyles = {
 };
 const nameWrapperStyles = {
   display: 'flex',
-  justifyContent: 'center',
+  justifyContent: 'flex-end',
   alignItems: 'center',
-  gap: '2vw',
+  gap: '.5vw',
+};
+const summaryDisplayStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-end',
+  alignItems: 'flex-start',
+  marginTop: '1.5vh',
+  gap: '.5vh',
+  marginLeft: '12vw',
 };

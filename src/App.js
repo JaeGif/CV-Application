e3c05e20267c85ecd2bf399a6908facc3cc7b479
Jae <giffordjacob0@gmail.com>
2022-10-24 14:19:48 +0800

    experience conditionally renders in range up to 4

--- src/App.js
+++ src/App.js
@@ -6,19 +6,54 @@ import ProfileImage from './components/ProfileImage';
 import Skills from './components/Skills';
 import Languages from './components/Languages';
 import Experience from './components/Experience';
+import { Component } from 'react';
+import uniqid from 'uniqid';
 
-function App() {
-  return (
-    <div>
-      <ProfileImage></ProfileImage>
-      <PersonalInfo></PersonalInfo>
-      <Contact></Contact>
-      <Education></Education>
-      <Skills></Skills>
-      <Languages></Languages>
-      <Experience></Experience>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      experienceCount: 0,
+    };
+  }
+
+  addExperience = () => {
+    if (this.state.experienceCount === 4) {
+      alert('No more than 4 jobs may be added');
+      return;
+    }
+    this.setState({
+      experienceCount: this.state.experienceCount + 1,
+    });
+  };
+  removeExperience = () => {
+    this.setState({
+      experienceCount: this.state.experienceCount - 1,
+    });
+  };
+
+  render() {
+    const experience = [];
+
+    for (let i = 0; i < this.state.experienceCount; i++) {
+      experience.push(<Experience key={uniqid()} number={i} />);
+    }
+    return (
+      <div>
+        <ProfileImage></ProfileImage>
+        <PersonalInfo></PersonalInfo>
+        <Contact></Contact>
+        <Education></Education>
+        <Skills></Skills>
+        <Languages></Languages>
+
+        <div>
+          {experience}
+          <button onClick={this.addExperience}>Add Work</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import ExperienceRender from './ExperienceRender';
+
 export class Experience extends Component {
   constructor() {
     super();
     this.state = {
-      job: {
-        company: 'Google',
-        jobTitle: 'Sr. Software Engineer',
-        start: '2016',
-        end: '2020',
-        city: 'Seattle',
-        duties:
-          "I did all of these things at my last job, but it's not a list because the developer doesn't like making everything a list on a resume.",
-      },
-      jobs: [],
+      company: 'Google',
+      jobTitle: 'Sr. Software Engineer',
+      start: '2016',
+      end: '2020',
+      city: 'Seattle',
+      duties:
+        "I did all of these things at my last job, but it's not a list because the developer doesn't like making everything a list on a resume.",
       isEdit: false,
     };
   }
 
+  onCompanyChange = (e) => {
+    this.setState({
+      company: e.target.value,
+    });
+  };
+
+  onStartChange = (e) => {
+    this.setState({
+      start: e.target.value.slice(0, 4),
+    });
+  };
+
+  onEndChange = (e) => {
+    this.setState({
+      end: e.target.value.slice(0, 4),
+    });
+  };
+
+  onCityChange = (e) => {
+    this.setState({
+      city: e.target.value,
+    });
+  };
+
+  onDutiesChange = (e) => {
+    this.setState({
+      duties: e.target.value,
+    });
+  };
+
+  onJobTitleChange = (e) => {
+    this.setState({
+      jobTitle: e.target.value,
+    });
+  };
+
+  changeEdit = () => {
+    console.log('click');
+    if (this.state.isEdit) {
+      this.setState({
+        isEdit: false,
+      });
+    } else {
+      this.setState({
+        isEdit: true,
+      });
+    }
+  };
+
   render() {
-    const { job, jobs, isEdit } = this.state;
+    const { company, jobTitle, start, end, city, duties, isEdit } = this.state;
 
     return (
       <div>
@@ -28,36 +74,66 @@ export class Experience extends Component {
             <div>
               <label htmlFor='company'>Company</label>
               <input
+                onChange={this.onCompanyChange}
                 name='company'
                 type='text'
-                placeholder={job.company}
+                placeholder={company}
               ></input>
             </div>
             <div>
               <label htmlFor='title'>Title</label>
               <input
+                onChange={this.onJobTitleChange}
                 name='title'
                 type='text'
-                placeholder={job.jobTitle}
+                placeholder={jobTitle}
               ></input>
             </div>
             <div>
               <label htmlFor='start'>Start Date</label>
-              <input name='start' type='date' placeholder={job.start}></input>
+              <input
+                onChange={this.onStartChange}
+                name='start'
+                type='date'
+                placeholder={start}
+              ></input>
             </div>
             <div>
               <label htmlFor='end'>End Date</label>
-              <input name='end' type='date' placeholder={job.end}></input>
+              <input
+                onChange={this.onEndChange}
+                name='end'
+                type='date'
+                placeholder={end}
+              ></input>
             </div>
             <div>
               <label htmlFor='duties'>Job Duties</label>
-              <textarea name='duties' placeholder={job.duties}></textarea>
+              <textarea
+                onChange={this.onDutiesChange}
+                name='duties'
+                placeholder={duties}
+              ></textarea>
             </div>
-            <button type='submit'>Submit</button>
+            <button onClick={this.changeEdit} type='button'>
+              Add
+            </button>
           </form>
         ) : (
-          <div>
-            <ExperienceRender job={job}></ExperienceRender>
+          <div onClick={this.changeEdit}>
+            <span>
+              <h3>{company}</h3>
+              <h3>{jobTitle}</h3>
+            </span>
+            <span>
+              <span>
+                <p>{start}</p>
+                <p>-</p>
+                <p>{end}</p>
+              </span>
+              <p>{city}</p>
+            </span>
+            <p>{duties}</p>
           </div>
         )}
       </div>
